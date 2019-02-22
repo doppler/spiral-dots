911eba896cf0ce2bc40e0900aee35c2130b14068
David Rose <doppler@gmail.com>
2019-02-21 22:56:57 -0500

    Updates

---
+++ precache-manifest.e5f0984ac4c875f75e2968844939c477.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9aabc2bd235f3b654b27",
+    "url": "/spiral-dots/static/css/main.c96b3be5.chunk.css"
+  },
+  {
+    "revision": "9aabc2bd235f3b654b27",
+    "url": "/spiral-dots/static/js/main.9aabc2bd.chunk.js"
+  },
+  {
+    "revision": "c92368f363362d87650d",
+    "url": "/spiral-dots/static/js/runtime~main.c92368f3.js"
+  },
+  {
+    "revision": "62638be1531f8ab7d36d",
+    "url": "/spiral-dots/static/js/2.62638be1.chunk.js"
+  },
+  {
+    "revision": "b0827605ae18de14b66e330a4315b41c",
+    "url": "/spiral-dots/index.html"
+  }
+];
